b0921ce8cb2a67e6120f13f34394af346d540359
Safiyyah123 <163822823+Safiyyah123@users.noreply.github.com>
2024-07-25 09:34:10 +0200

    text edit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!</Text>
+      <Text>Hello world!! my name is xxx</Text>
       <StatusBar style="auto" />
     </View>
   );
